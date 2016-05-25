8a79c52cedcf85effd94a5d8394625b0c706c814
Arjo Ghosh আর্য্য ঘোষ <arjoghosh@outlook.com>
2016-05-25 17:48:47 +0530

    Create viva-broadband-connector.py

---
+++ viva-broadband-connector.py
@@ -0,0 +1,15 @@
+import requests
+a = requests.get('http://10.10.0.1/indexmain.php')
+x=input("Enter username : ")
+x=x.strip()
+y=input("Enter password : ")
+y=y.strip()
+params = {'txtUserID': x, 'provider': 'smartguard', 'txtPassword':y, 'website_name':'',
+          'MM_insert':'frmLogon', 'Submit':'Sign In'}
+r = requests.post("http://10.10.0.1/make_me_login.php", data=params,cookies=a.cookies)
+q=requests.get("http://10.10.0.1/signin.php?web_name=",cookies=a.cookies)
+t=requests.get("http://10.10.0.1/clientmain.php",cookies=a.cookies)
+if t.status_code==200:
+    print('Your Internet connection is configured properly.')
+else:
+   print('Something went wrong. Please try again later.')
